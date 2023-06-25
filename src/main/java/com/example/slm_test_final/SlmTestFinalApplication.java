440d95895643b16923363937f3f2e66acc6e5e6b
Markus Koppensteiner <wi22b010@technikum-wien.at>
2023-06-25 18:28:35 +0200

    initial commit - Projekt erstellt

---
+++ src/main/java/com/example/slm_test_final/SlmTestFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_test_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmTestFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slm_test_final/SlmTestFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_test_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
